658db0a7b2c4cfd08433299444dbbc4f80a6b110
mohamadhs7 <mohamaddhs7@gmail.com>
2021-11-14 16:03:21 +0330

    16:02 11/14/2021 changes

--- src/test/java/com/jrp/pma/dao/ProjectRepositoryIntegrationTest.java
+++ src/test/java/com/jrp/pma/dao/ProjectRepositoryIntegrationTest.java
@@ -1,17 +1,22 @@
 package com.jrp.pma.dao;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.jrp.pma.entities.Project;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@SqlGroup({@Sql(executionPhase = ExecutionPhase.BEFORE_TEST_METHOD,scripts = {"classpath:schema.sql","classpath:data.sql"})
+,@Sql(executionPhase = ExecutionPhase.AFTER_TEST_METHOD,scripts = {"classpath:drop.sql"})})
 public class ProjectRepositoryIntegrationTest {
 	
 	@Autowired
@@ -21,9 +26,6 @@ public class ProjectRepositoryIntegrationTest {
 	public void ifNewProjectSaved_thenSuccess() {
 		Project newProject = new Project("New Test Project","COMPLETE","Test Description");
 		proRepo.save(newProject);
-		
-		assertEquals(1,proRepo.findAll().size());
-		
-		
+		assertEquals(5,proRepo.findAll().size());
 	}
 }
