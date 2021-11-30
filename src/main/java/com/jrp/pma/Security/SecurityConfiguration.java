746f4179fe4ea00f7e2f4393ec2c727ba180befb
mohamad <mohamaddhs7@gmail.com>
2021-11-30 23:42:22 +0330

    Spring Security Encryption

--- src/main/java/com/jrp/pma/Security/SecurityConfiguration.java
+++ src/main/java/com/jrp/pma/Security/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package com.jrp.pma.Security;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import javax.activation.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +13,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -17,36 +22,49 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Autowired
-	DataSource datasource;
+	javax.sql.DataSource datasource;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptEncoder;
 		
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		//auth.inMemoryAuthentication()
-			auth.jdbcAuthentication().dataSource(datasource)
-			.withDefaultSchema()
-			.withUser("mohamad")
-			.password("zedanesh79")
-			.roles("USER")
-			.and()
-			.withUser("usr")
-			.password("zedanesh79")
-			.roles("ADMIN");
-			
+	//	auth.inMemoryAuthentication()
+//			auth.jdbcAuthentication().dataSource(datasource)
+//			.withDefaultSchema()
+//			.withUser("mohamad")
+//			.password("zedanesh79")
+//			.roles("USER")
+//			.and()
+//			.withUser("usr")
+//			.password("zedanesh79")
+//			.roles("ADMIN");
+		
+		auth.jdbcAuthentication()
+		.usersByUsernameQuery("select username, password, enabled"+
+		"from user_accounts where username = ?" )
+		.authoritiesByUsernameQuery("select username, role"+
+		"from user_accounts where username = ?")
+		.dataSource(datasource)
+		.passwordEncoder(bCryptEncoder);		
 	}
-	@Bean
-	public PasswordEncoder getPasswordEncoder() {
-		return NoOpPasswordEncoder.getInstance();
-	}
+	
+//	@Bean
+//	public PasswordEncoder getPasswordEncoder() {
+//		return NoOpPasswordEncoder.getInstance();
+//	}
 	
 	protected void configure (HttpSecurity http)throws Exception{
 		http.authorizeRequests()
 			.antMatchers("/projects/new").hasRole("ADMIN")
+			.antMatchers("/projects/save").hasRole("ADMIN")
 			.antMatchers("/employees/new").hasRole("ADMIN")
-			.antMatchers("/h2_console/**").permitAll()
-			.antMatchers("/").authenticated().and().formLogin();
+			.antMatchers("/employees/save").hasRole("ADMIN")
+			//.antMatchers("/h2_console/**").permitAll()
+			.antMatchers("/","/**").permitAll()
+			.and()
+			.formLogin();
 		
-		http.csrf().disable();
-		http.headers().frameOptions().disable();
+//		http.csrf().disable();
+//		http.headers().frameOptions().disable();
 	}
-	
-
 }
